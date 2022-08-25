1bb403525afebcf06c4c72efcc09f6c02f3b9ea3
dagkien <kiendang712002@gmail.com>
2022-08-25 17:04:08 +0700

    setup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,8 +70,6 @@ const config = {
             label: "Tutorial",
           },
           { to: "/blog", label: "Blog", position: "left" },
-          { to: "/about", label: "About", position: "left" },
-          { to: "/member", label: "Member", position: "left" },
           {
             href: "https://github.com/facebook/docusaurus",
             label: "GitHub",
